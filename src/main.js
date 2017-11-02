db52352bbc8fe9f0cd77597717c3b453e00db294
Luiz Gonçalves <luiz.goncalves@ufpr.br>
2017-11-02 16:18:30 -0200

    add navbar

--- src/main.js
+++ src/main.js
@@ -1,44 +1,44 @@
 var modelos = {
-    "c1": [
-        {
-           "nome": "Classic 01",
-           "preco": "1.501,00"
-        },
-        {
-           "nome": "Classic 02",
-           "preco": "1.502,00"
-        }
-    ],
-    "c2": [
-        {
-           "nome": "Moderno 01",
-           "preco": "1.601,00"
-        },
-        {
-           "nome": "Moderno 02",
-           "preco": "1.602,00"
-        }
-    ],
-    "c3": [
-        {
-           "nome": "Lancha 01",
-           "preco": "1.701,00"
-        },
-        {
-           "nome": "Lancha 02",
-           "preco": "1.702,00"
-        }
-    ],
-    "c4": [
-        {
-           "nome": "Helicóptero 01",
-           "preco": "1.801,00"
-        },
-        {
-           "nome": "Helicóptero 02",
-           "preco": "1.802,00"
-        }
-    ]
+  "c1": [
+  {
+   "nome": "Classic 01",
+   "preco": "1.501,00"
+ },
+ {
+   "nome": "Classic 02",
+   "preco": "1.502,00"
+ }
+ ],
+ "c2": [
+ {
+   "nome": "Moderno 01",
+   "preco": "1.601,00"
+ },
+ {
+   "nome": "Moderno 02",
+   "preco": "1.602,00"
+ }
+ ],
+ "c3": [
+ {
+   "nome": "Lancha 01",
+   "preco": "1.701,00"
+ },
+ {
+   "nome": "Lancha 02",
+   "preco": "1.702,00"
+ }
+ ],
+ "c4": [
+ {
+   "nome": "Helicóptero 01",
+   "preco": "1.801,00"
+ },
+ {
+   "nome": "Helicóptero 02",
+   "preco": "1.802,00"
+ }
+ ]
 };
 
 
@@ -68,7 +68,7 @@ document.querySelector('#categoria').onchange = function() {
 }
 
 document.querySelector('#modelos').onchange = function() {
-    document.getElementById("preco").innerHTML = this.value;
+  document.getElementById("preco").innerHTML = this.value;
 }
 
 
@@ -81,4 +81,57 @@ for (i = 0; i < dots.length; i++) {
     document.getElementsByClassName("active")[0].classList.remove("active");
     this.classList.add('active');
   });
+}
+
+document.onscroll = function() {
+
+  if (isViewable(document.getElementById("section-quemsomos"))) {
+
+    var actives = document.getElementsByClassName("active");
+    if (actives.length > 0) {
+      actives[0].classList.remove("active");
+      document.getElementById("dot1").classList.add('active');
+    }
+
+  }
+  else if (isViewable(document.getElementById("section-galeria"))) {
+
+    var actives = document.getElementsByClassName("active");
+    if (actives.length > 0) {
+      actives[0].classList.remove("active");
+      document.getElementById("dot2").classList.add('active');
+    }
+
+  }
+  else if (isViewable(document.getElementById("section-orcamento"))) {
+
+    var actives = document.getElementsByClassName("active");
+    if (actives.length > 0) {
+      actives[0].classList.remove("active");
+      document.getElementById("dot3").classList.add('active');
+    }
+
+  }
+
+};
+
+
+function isViewable(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top < (window.pageYOffset + window.innerHeight) &&
+    left < (window.pageXOffset + window.innerWidth) &&
+    (top + height) > window.pageYOffset &&
+    (left + width) > window.pageXOffset
+    );
 }
